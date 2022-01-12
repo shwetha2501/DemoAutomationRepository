135517843369fd2a0cdb17b3aa10244cd4484dc6
swetha <shwethabs25@gmail.com>
2022-01-12 22:10:12 +0530

    WhileLoop

---
+++ Web-Automation/src/com/sgtesting/Framework/Sample4.java
@@ -0,0 +1,16 @@
+package com.sgtesting.Framework;
+
+public class Sample4 
+{
+	public static void main (String [] args)
+	{
+		int i=1;
+		while(i<=20)
+		{
+			System.out.println("Display the number:"+i);
+			i++;
+			}
+			
+	}
+
+}
